6e94bbe7ec112738568585ca804f0594c2da591a
hirokinishimoto <hirokinishimoto@hirokinishimotonoMacBook-Pro.local>
2019-02-20 11:40:38 +0900

    Viewの追加

--- Server/item/models.py
+++ Server/item/models.py
@@ -1,9 +1,18 @@
 from django.db import models
- 
+from rest_framework import serializers
+
 
 # Create your models here.
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=30)
-    count = models.IntegerField(max_length=30)
+    name = models.CharField(max_length=30, null=True)
+    count = models.IntegerField(max_length=30, null=True)
+    photo = models.ImageField(upload_to='images/', null=True)
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ('name', 'count', 'photo')
+
